01511aface176cb5f7afe8719ac6349f3bab95a0
thapaRoyal <thaparoyal17@gmail.com>
2022-07-24 17:57:22 +0545

    implementinh dark mode

--- App.tsx
+++ App.tsx
@@ -1,19 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Switch } from 'react-native';
+import { ThemeContext } from './src/context/ThemeContext';
+import { myColors } from './src/styles/Colors';
 
 export default function App() {
+  const [theme, setTheme] = useState('light');
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeContext.Provider value={theme}>
+      <View
+        style={
+          theme === 'light'
+            ? styles.container
+            : [styles.container, { backgroundColor: '#000' }]
+        }
+      >
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+        <Switch
+          value={theme === 'light'}
+          onChange={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+        />
+      </View>
+    </ThemeContext.Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: myColors.light,
     alignItems: 'center',
     justifyContent: 'center',
   },
